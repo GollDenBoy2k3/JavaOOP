587e60a43df13608573f8f8319ea9f01cdfbf873
GollDenBoy2k3 <quybga@gmail.com>
2023-03-08 07:18:23 +0700

    first commit

---
+++ baithuchanh0103/src/slide40.java
@@ -0,0 +1,24 @@
+public class slide40 {
+    public static void main (string []arg)
+    scanner lich = new scanner (system.in)
+}
+int chat;
+system.out.println (x:"Nhap vao gia tri tu 1 den 7:");
+chat = lich.nextInt();
+switch (chat){
+    case 1 : System.out.println("chu nhat");
+    break;
+    case 2 : System.out.println("thu hai");
+    break;
+    case 3 : System.out.println("thu ba");
+    break;
+    case 4 : System.out.println("thu tu");
+    break;
+    case 5 : System.out.println("thu nam");
+    break;
+    case 6 : System.out.println("thu sau");
+    break;
+    case 7 : System.out.println("thu bay");
+    break;
+    default : system.out.println (x: "khong phai ngay trong tuan");
+}
---
+++ laptrinhcoban/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("duc anh an buoi");
+    }
+}
